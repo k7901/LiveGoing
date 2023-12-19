1647a749f22d108543fccd1bac561d883e206037
KokiOhara <144040393+k7901@users.noreply.github.com>
2023-12-19 20:25:00 +0000

    途中

--- app/controllers/public/searches_controller.rb
+++ app/controllers/public/searches_controller.rb
@@ -1,2 +1,17 @@
 class Public::SearchesController < ApplicationController
+
+  def index
+    # 検索ロジックをここに実装
+    @hotels = Hotel.search(search_params)
+
+    # 検索パラメータをセッションに保存
+    session[:search_params] = search_params
+  end
+
+  private
+
+  def search_params
+    params.permit(:check_in_date, :check_out_date, :guests_count, :rooms_count)
+  end
+
 end
--- app/models/hotel.rb
+++ app/models/hotel.rb
@@ -6,6 +6,31 @@ class Hotel < ApplicationRecord
   #has_one_attached :image
   has_many_attached :images
 
+  scope :search, -> (search_params) do
+    return if search_params.blank?
+
+    venue_like(search_params[:venue])
+      .check_in_out_range(search_params[:check_in_date], search_params[:check_out_date])
+      .guest(search_params[:guest])
+      .amount(search_params[:amount])
+  end
+
+  scope :venue_like, -> (venue_name) { joins(:venue).where('venues.name LIKE ?', "%#{venue_name}%") if venue_name.present? }
+  scope :check_in_out_range, -> (check_in_date, check_out_date) {
+    where('check_in_date <= ? AND check_out_date >= ?', check_in_date, check_out_date) if check_in_date.present? && check_out_date.present?
+  }
+  scope :guest, -> (guest) { joins(:rooms).where('rooms.capacity >= ?', guest) if guest.present? }
+  scope :amount, -> (amount) { joins(:rooms).where('rooms.capacity >= ?', amount) if amount.present? }
+
+
+
+  #def self.search(search_params)
+    #hotels = Hotel.all
+    #if search_params[:venue].present?
+     # hotels = hotels.joins(:venue).where("venues.name LIKE ?", "%#{search_params[:venue]}%")
+    #end
+    #hotels
+  #end
 
 
   def get_image(width, height)
