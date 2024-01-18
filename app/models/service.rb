b9fe188dca159b8b533970f2cff331dab7b23999
KokiOhara <144040393+k7901@users.noreply.github.com>
2024-01-18 10:55:09 +0000

    readme

--- app/models/service.rb
+++ app/models/service.rb
@@ -1,4 +1,7 @@
 class Service < ApplicationRecord
   has_many :hotel_service_relations
   has_many :hotels, through: :hotel_service_relations
+  
+  validates :name, presence: true
+
 end
--- app/models/venue.rb
+++ app/models/venue.rb
@@ -1,4 +1,8 @@
 class Venue < ApplicationRecord
    belongs_to :prefecture
    has_many :hotels
+   
+  validates :last_name, presence: true
+  validates :first_name, presence: true
+  validates :email, presence: true
 end
